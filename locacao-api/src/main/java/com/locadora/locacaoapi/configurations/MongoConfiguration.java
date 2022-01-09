a56b250b62573ffb2957964c21fa4e4bd8d310f2
hyan.campos <hyan.campos@luizalabs.com>
2022-01-09 18:38:50 -0300

    Configurations all project

--- locacao-api/src/main/java/com/locadora/locacaoapi/configurations/MongoConfiguration.java
+++ locacao-api/src/main/java/com/locadora/locacaoapi/configurations/MongoConfiguration.java
@@ -8,4 +8,6 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @EnableMongoAuditing
 @EnableMongoRepositories(basePackages = {"com.locadora.locacaoapi.repositorys"})
 public class MongoConfiguration {
+    public MongoConfiguration() {
+    }
 }
